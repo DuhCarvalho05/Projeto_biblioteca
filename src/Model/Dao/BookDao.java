580c4509381b14d4a0bf7339dd5f0ce0682f6374
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-03-21 21:38:23 -0300

    Solving setBorrow bug in LoanDao.

--- src/Model/Dao/BookDao.java
+++ src/Model/Dao/BookDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface BookDao {
     boolean insert(Book book);
+    boolean update(Book book);
     boolean delete(Book book);
     List<Book> getAll();
     Book getById(String title, int edition);
